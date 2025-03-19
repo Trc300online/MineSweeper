c8aee8333cdf675e7daa7ec542143ac5f0009d4b
Trc300online <186137334+Trc300online@users.noreply.github.com>
2025-03-19 12:24:49 +0100

    added Switch/case to main logic

--- MineSweeper/src/Game.java
+++ MineSweeper/src/Game.java
@@ -5,12 +5,58 @@ public class Game {
         Board board = new Board();
         board.placeBombs(board.gameBoard);
         board.countBombAround();
-
         boolean gameContinues = true;
+
         while (gameContinues) {
+
             Screen.printBoard(board);
             char action = Screen.askAction();
-            if (action == 'Q' || action == 'q') {
+
+            switch (action) {
+                case 'Q':
+                case 'q':
+                    gameContinues = board.quit();
+                    break;
+
+
+                case 'R':
+                case 'r':
+                    int columns = Screen.getColumnCords();
+                    int rows = Screen.getRowCords();
+
+                    if (board.gameOver(rows, columns)){
+
+                        board.revealAll();
+                        Screen.printBoard(board);
+                        Screen.gameOverMsg();
+                        break;
+                    }
+                    if (board.revealable(rows, columns) && !board.gameBoard[rows][columns].isFlagged) {
+
+                        board.gameBoard[rows][columns].reveal();
+                        board.revealNeighbours(rows, columns);
+
+                        if (board.winCond(rows, columns)) {
+                            Screen.printBoard(board);
+                            Screen.winMsg();
+                            break;
+                        }
+                    }
+                    break;
+
+
+                case 'F':
+                case 'f':
+                    int columnsF = Screen.getColumnCords();
+                    int rowsF = Screen.getRowCords();
+
+                    if (board.flaggeable(rowsF, columnsF)) {
+
+                        board.gameBoard[rowsF][columnsF].toggleFlag();
+                    }
+                    break;
+            }
+            /*if (action == 'Q' || action == 'q') {
                 gameContinues = board.quit();
             }
             if (action == 'R' || action == 'r'){
@@ -38,7 +84,7 @@ public class Game {
                 if (board.flaggeable(rows, columns)) {
                     board.gameBoard[rows][columns].toggleFlag();
                 }
-            }
+            }*/
         }
     }
 }
