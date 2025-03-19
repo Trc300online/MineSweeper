d7c8983a90a343edebea709bc637ca4329a06275
Trc300online <186137334+Trc300online@users.noreply.github.com>
2025-03-19 12:44:32 +0100

    delete commented code

--- MineSweeper/src/Game.java
+++ MineSweeper/src/Game.java
@@ -57,35 +57,6 @@ public class Game {
                     }
                     break;
             }
-            /*if (action == 'Q' || action == 'q') {
-                gameContinues = board.quit();
-            }
-            if (action == 'R' || action == 'r'){
-                int columns = Screen.getColumnCords();
-                int rows = Screen.getRowCords();
-                if (board.gameOver(rows, columns)){
-                    board.revealAll();
-                    Screen.printBoard(board);
-                    Screen.gameOverMsg();
-                    break;
-                }
-                if (board.revealable(rows, columns) && !board.gameBoard[rows][columns].isFlagged) {
-                    board.gameBoard[rows][columns].reveal();
-                    board.revealNeighbours(rows, columns);
-                    if (board.winCond(rows, columns)) {
-                        Screen.printBoard(board);
-                        Screen.winMsg();
-                        break;
-                    }
-                }
-
-            } else if (action == 'F' || action == 'f') {
-                int columns = Screen.getColumnCords();
-                int rows = Screen.getRowCords();
-                if (board.flaggeable(rows, columns)) {
-                    board.gameBoard[rows][columns].toggleFlag();
-                }
-            }*/
         }
     }
 }
